507bf46509c30e186e427491bd56664f169fb3d5
ferdinand <mayfen19@htl-kaindorf.at>
2024-03-12 12:04:03 +0100

    test

--- src/robu/setup.py
+++ src/robu/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+import os
+from glob import glob
 package_name = 'robu'
 
 setup(
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,10 +22,14 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ex10_wallfollower = robu.ex10_wallfollower:main',
+            'wallfollower = robu.ex10_wallfollower:main',
             'mypublisher = robu.publisher_member_function:main',
             'mysubscriber = robu.subscriber_member_function:main',
-            'myparameter = robu.ex11_parameter:main'
+            'myparameter = robu.ex11_parameter:main',
+            'testparameter = robu.test_parameter:main',
+            'testparameter_get = robu.test_parameter_get:main',
+            'fibonacci_server = robu.ex12_fibonacci_server:main',
+            'fibonacci_client = robu.ex12_fibonacci_client:main'
         ],
     },
 )
